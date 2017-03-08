acd07ed81e56ce51119dc5ac777596f08b68d61d
Backbase <ajpaul89@gmail.com>
2017-03-08 08:41:33 -0500

    Added more bootstrap table logic

--- app/repeater-test/repeater-test.js
+++ app/repeater-test/repeater-test.js
@@ -1,12 +1,48 @@
 angular.module('myApp.repeater-test', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/repeater-test', {
-    templateUrl: 'repeater-test/repeater-test.html',
-    controller: 'RepeaterTestCtrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/repeater-test', {
+      templateUrl: 'repeater-test/repeater-test.html',
+      controller: 'RepeaterTestCtrl'
+    });
+  }])
 
-.control('RepeaterTestCtrl', function($scope) {
-    
+  .controller('RepeaterTestCtrl', function ($scope) {
+    $scope.records = [
+      "Alfreds Futterkiste",
+      "Berglunds snabbköp",
+      "Centro comercial Moctezuma",
+      "Ernst Handel",
+    ];
+
+    $scope.colors = [
+      {
+        color: "red",
+        value: "#f00"
+      },
+      {
+        color: "green",
+        value: "#0f0"
+      },
+      {
+        color: "blue",
+        value: "#00f"
+      },
+      {
+        color: "cyan",
+        value: "#0ff"
+      },
+      {
+        color: "magenta",
+        value: "#f0f"
+      },
+      {
+        color: "yellow",
+        value: "#ff0"
+      },
+      {
+        color: "black",
+        value: "#000"
+      }
+    ]
 });
